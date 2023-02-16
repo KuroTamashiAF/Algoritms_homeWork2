2c903593d493d368848c018b6c554d575697c4ef
Fedorov <chugun777akk@mail.ru>
2023-02-16 19:00:55 +0700

    add class ArrayPreparation

---
+++ HomeWork_2/src/ArrayPreparation.java
@@ -0,0 +1,22 @@
+public class ArrayPreparation {
+
+    public int[] fillArray(int [] arr)
+    {
+        if (arr.length == 0){
+            throw new RuntimeException("Пришли крывые данные");
+        }
+        for( int i =0;  i <arr.length; i++){
+            arr[i] = (int) Math.random()* 100;
+        }
+        return arr;
+    }
+    public void PrintArray(int[] arr){
+        for (int i = 0; i <arr.length; i++ ){
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+    public void separator(){
+        System.out.println("--------------------------------------------");
+    }
+}
