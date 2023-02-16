100511976d78dddeae81bf89d7d2d72f17a79b8e
Fedorov <chugun777akk@mail.ru>
2023-02-16 21:11:12 +0700

    Add Heapify

--- HomeWork_2/src/UserSort.java
+++ HomeWork_2/src/UserSort.java
@@ -4,6 +4,21 @@ public class UserSort {
         int theLargest  = rootIndex;
         int leftChild  = 2*rootIndex + 1;
         int rightChild  =  2*rootIndex+2;
-        
+
+        if (leftChild< heapSize && arr[leftChild] > arr[theLargest])
+            theLargest = leftChild;
+        if (rightChild <heapSize && arr[rightChild]> arr[theLargest] )
+            theLargest = rightChild;
+
+        if (theLargest !=rootIndex){
+            int temp  = arr[rootIndex];
+            arr[rootIndex] = arr[theLargest];
+            arr[theLargest] = temp;
+        }
+        heapIfy(arr,heapSize, theLargest );
+
+
+
+
     }
 }
